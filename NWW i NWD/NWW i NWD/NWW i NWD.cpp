a435b63341c2690a0ac1b6a8bffb5da773cb339d
Bartakt741 <liswlesiexx@gmail.com>
2022-03-15 13:14:19 +0100

    nwd i nww

---
+++ NWW i NWD/NWW i NWD/NWW i NWD.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	int iloczyn;
+	cout << "podaj pierwszą liczbę :";
+	cin >> a;
+	cout << "podaj drugą liczbę :";
+	cin >> b;
+	while(a != b)  {
+		if (a < b) {
+			b = b - a;
+		}
+		else{
+			a = a - b;
+		}
+	}
+	cout << " to jest NWD :";
+	cout << a;
+
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+	}
+
+}
+
